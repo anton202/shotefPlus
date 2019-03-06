33ad95716ca51882ce8994b2a59cb6fc7e3ece0c
anton202 <a.kluge202@gmail.com>
2019-03-06 13:15:59 +0200

    add header table

--- client/src/app/search-results/search-results.component.ts
+++ client/src/app/search-results/search-results.component.ts
@@ -16,13 +16,35 @@ export class SearchResultsComponent implements OnInit {
     shotefPlus: 60,
     daysOfPAymentDelay: 30,
     comment:'החברה תמיד מאחרת בתשלומים שלה ממולץ לעלות להם את המחיר על העבודה עקב האיחרוים בתשלומים'
+  },
+  {
+    publisherName:'שלג הנדסה',
+    shotefPlus: 60,
+    daysOfPAymentDelay: 30,
+    comment:'החברה תמיד מאחרת בתשלומים שלה ממולץ לעלות להם את המחיר על העבודה עקב האיחרוים בתשלומים'
+  },
+  {
+    publisherName:'שלג הנדסה',
+    shotefPlus: 60,
+    daysOfPAymentDelay: 30,
+    comment:'החברה תמיד מאחרת בתשלומים שלה ממולץ לעלות להם את המחיר על העבודה עקב האיחרוים בתשלומים'
   },{
     publisherName:'שלג הנדסה',
     shotefPlus: 60,
     daysOfPAymentDelay: 30,
     comment:'החברה תמיד מאחרת בתשלומים שלה ממולץ לעלות להם את המחיר על העבודה עקב האיחרוים בתשלומים'
-  }]
-  columnsToDisplay = ['שם מפרסם הדוח','שוטף פלוס','יימי איחור אחרי תקופת שוטף פלוס','הערה']
+  },
+  {
+    publisherName:'שלג הנדסה',
+    shotefPlus: 60,
+    daysOfPAymentDelay: 30,
+    comment:'החברה תמיד מאחרת בתשלומים שלה ממולץ לעלות להם את המחיר על העבודה עקב האיחרוים בתשלומים'
+  }
+]
+  headerData = [{name:'בתי זיקוק בעמ',number:51000043,shotefPlusAvrg:90,avrgDaysOfDelay:100}];
+
+  columnsToDisplay = ['שם מפרסם הדוח','שוטף פלוס','יימי איחור אחרי תקופת שוטף פלוס','הערה'];
+  headerColumns = ['שם חברה','מספר חברה','ממוצע שוטף פלוס','ממוצע ימיי איחור'];
   constructor() { }
 
   ngOnInit() {
