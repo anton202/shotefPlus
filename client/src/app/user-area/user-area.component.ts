93a8f7ac66924f083bb5150e593d3fee2eee7a0c
anton202 <a.kluge202@gmail.com>
2019-03-19 21:52:37 +0200

    add success message and error message when deleting or changing report

--- client/src/app/user-area/user-area.component.ts
+++ client/src/app/user-area/user-area.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserAreaService } from './user-area.service';
-import { MatDialog } from '@angular/material';
-import { ConfirmActionComponent } from '../confirm-action/confirm-action.component';
 
 @Component({
   selector: 'app-user-area',
@@ -9,10 +7,10 @@ import { ConfirmActionComponent } from '../confirm-action/confirm-action.compone
   styleUrls: ['./user-area.component.css']
 })
 export class UserAreaComponent implements OnInit {
-  records = [{name:'בתי זיקוק לנפט בעמ',records:[{shotefPlus:30,delay:60,comment:'פעם אחת לא קיבלתי את התשלום מהחברה הזאת בזמן',createdAt:'20/3/2019',_id:'1543hnxsjo45'}]}];
+  records = [{ name: 'בתי זיקוק לנפט בעמ', records: [{ shotefPlus: 30, delay: 60, comment: 'פעם אחת לא קיבלתי את התשלום מהחברה הזאת בזמן', createdAt: '20/3/2019', _id: '1543hnxsjo45' }] }];
   errorMessage;
-  proccesingSpinner = false;
-  constructor(private userAreaService: UserAreaService, private dialog: MatDialog) { }
+  
+  constructor(private userAreaService: UserAreaService) { }
 
   ngOnInit() {
     // this.userAreaService.getRecords()
@@ -22,19 +20,13 @@ export class UserAreaComponent implements OnInit {
     //     )
   }
 
-  onDelete(reportId){
-    const dailogRef = this.userAreaService.openConfirmationDialog('למחוק דיווח?')
-
-    dailogRef.afterClosed().subscribe((deleteReport)=>{
-      if(deleteReport){
-      this.userAreaService.deleteReport(reportId)
-        .subscribe(
-          ()=> console.log('deleted'),
-          error => console.log('erroe')
-          )
+  onButtonClick(reportId,text,actionType,report) {
+    this.userAreaService.confirmAction(text)
+      .subscribe((actionConfirmed) => {
+        if (actionConfirmed) {
+          this.userAreaService.changeReportSet(actionType,reportId,report)
         }
-    })
-  
+      })
   }
 
 }
--- client/src/app/user-area/user-area.service.ts
+++ client/src/app/user-area/user-area.service.ts
@@ -11,10 +11,36 @@ export class UserAreaService {
     apiUrl = environment.apiUrl;
     token = localStorage.getItem('token')
     headers = new HttpHeaders().set('Authorization',this.token);
+    proccesingSpinner = false;
+    successMessage = false;
+    errorMessage = false;
     constructor(private http: HttpClient, private dailog: MatDialog) { }
 
-    openConfirmationDialog(text){
-       return this.dailog.open(ConfirmActionComponent,{data:{text}})
+    confirmAction(text){
+       const dialogRef = this.dailog.open(ConfirmActionComponent,{data:{text}});
+       return dialogRef.afterClosed()
+    }
+
+    changeReportSet(actionType,reportId,record){
+        this.proccesingSpinner = true;
+        setTimeout(()=>{
+            this[actionType](reportId,record)
+            .subscribe(
+                ()=> {
+                    this.proccesingSpinner = false
+                    this.successMessage = true;
+                    setTimeout(()=>{
+                        this.successMessage = false
+                    },2500)
+                },
+                error => {
+                    this.proccesingSpinner = false;
+                    this.errorMessage = true;
+                    setTimeout(()=>this.errorMessage = false,3500)
+                }
+                )
+        },3000)
+        
     }
 
     getRecords(){
@@ -23,5 +49,9 @@ export class UserAreaService {
    
     deleteReport(reportId){
         return this.http.delete(`${this.apiUrl}/${reportId}`,{headers: this.headers})
+    }
+
+    saveChanges(reportId,report){
+       return this.http.put(`${this.apiUrl}/${reportId}`,report)
     }
 }
