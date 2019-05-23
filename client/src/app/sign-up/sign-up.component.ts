7b374dd4b7e2d1a4efdad885fe117fa963c33b30
anton202 <a.kluge202@gmail.com>
2019-05-23 14:09:10 +0300

    add handelresponse function in sign-up component

--- client/src/app/sign-up/sign-up.component.ts
+++ client/src/app/sign-up/sign-up.component.ts
@@ -1,39 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SignUpService } from './sign-up.service';
 import { MatDialogRef } from '@angular/material';
 import { registration } from './sign-up.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
-  
+
 })
-export class SignUpComponent  {
-  isSuccessfullyRegistrated: string; 
+export class SignUpComponent implements OnInit {
+  isSuccessfullyRegistrated: string;
   displaySpinner: boolean = false;
   statusMessage: string;
+  signUpForm: FormGroup;
 
-  constructor(private signUpService: SignUpService, private dialogRef:MatDialogRef<SignUpComponent>) { }
+  constructor(private signUpService: SignUpService, private dialogRef: MatDialogRef<SignUpComponent>) { }
 
-  public onClose(): void{
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'employmentType': new FormControl(null),
+      'freeLancerId': new FormControl(null, [Validators.required, Validators.minLength(9)]),
+      'companyId': new FormControl(null, Validators.required)
+    })
+  }
+
+  public onClose(): void {
     this.dialogRef.close()
   }
 
-  public onSignUp(formValues: registration): void{
+  public onSignUp(): void {
+    if (this.signUpForm.valid === false) {
+      return
+    }
     this.displaySpinner = true;
-    
-      this.signUpService.registerAccount(formValues)
-        .subscribe((response)=>{
-          this.displaySpinner = false;
-          this.isSuccessfullyRegistrated = 'success';
-          this.statusMessage = "נרשמתה בהצלחה, מייל ישלך אליך ברגע שהמשתמש יאומת"
-        },
-        error =>{
-          this.displaySpinner = false;
-          this.isSuccessfullyRegistrated = 'fail'
-          this.statusMessage = 'משהו השתבש, נסה שוב או פנה למפתח האתר'
+    this.signUpService.registerAccount(this.signUpForm.value)
+      .subscribe(() => {
+        this.handleRespone('success','נרשמתה בהצלחהת מייל ישלך אליך ברגע שהמשתמש יאומת')
+      },
+        error => {
+          this.handleRespone('fail','משהו השתבש, נסה שוב או פנה לתמיכה טכנית')
         })
+  }
+
+  private handleRespone(isSuccessfullyRegistrated: string,registrationStatusMessage:string): void{
+    this.displaySpinner = false;
+    this.isSuccessfullyRegistrated = isSuccessfullyRegistrated
+    this.statusMessage = registrationStatusMessage;
   }
 
 }
