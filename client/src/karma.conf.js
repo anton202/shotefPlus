88529c1fd3cb645f8fe5ec85a1e0d710cee861ee
anton202 <a.kluge202@gmail.com>
2019-06-24 12:38:22 +0300

    chande karma file

--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -1,18 +1,15 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-junit-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
-    reporters: ['junit'],    
+ reporters: ['junit'],    
     junitReporter: { 
  outputDir: 'karma-results',    
  outputFile: 'karma-results.xml'    
@@ -20,17 +17,10 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev',
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false
+ browsers: ['ChromeHeadless'],    
+ autoWatch: false,    
+ singleRun: true    
   });
 };
