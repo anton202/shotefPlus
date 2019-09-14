793573dd810e484229bcc3abcae29a885a697577
anton202 <a.kluge202@gmail.com>
2019-09-14 11:21:38 +0300

    testing github pages

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 
 
-
+// app.get('/',(req,res)=>{
+//     res.send('hello')
+// })
 app.use(express.static(path.join('./dist/client')))
 
-app.listen(8082,()=>console.log(8082))
+app.listen(8081,()=>console.log(8081))
